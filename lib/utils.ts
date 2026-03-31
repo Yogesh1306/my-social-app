efa9cb967cebfe55c17fc334d34b2cd918c7c60b
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2026-03-31 20:30:42 +0530

    feat: implement utils functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isBase64Image(imageData: string) {
+  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64/;
+  return base64Regex.test(imageData)
+}
+
+export async function base64ToFile(base64: string, fileName = "image.jpg"): Promise<File> {
+  const res = await fetch(base64);
+  const blob = await res.blob();
+  return new File([blob], fileName, { type: blob.type });
+}
