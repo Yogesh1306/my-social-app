697d83d83d328a26ce6f5ca9dc7723d9dbb07b0e
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2026-03-31 20:09:37 +0530

    feat: implemented sign-in page for user login

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="">
       <SignIn />
     </div>
   );
