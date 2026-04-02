db9687f62d7370d79f46914c89a0ac1add0bccea
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2026-04-02 16:43:10 +0530

    feat: enhance search functionality by adding search parameters to fetchUsers

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,28 +1,34 @@
 import UserCard from "@/components/cards/UserCard";
+import SearchBar from "@/components/shared/SearchBar";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   const user = await currentUser();
   if (!user) return null;
+  const { q } = await searchParams;
 
   const userInfo = await fetchUser(user?.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
+    searchString: q,
     pageNumber: 1,
     pageSize: 25,
   });
 
-  console.log({ result });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
 
       {/* search bar */}
+      <SearchBar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
